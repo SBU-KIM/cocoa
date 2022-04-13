4f4ccd140aa55e2fde203db0bf61cdc838673194
Vivian Miranda <vivianmiranda@arizona.edu>
2022-04-12 17:02:49 -0700

    updatE

--- Cocoa/cobaya/cobaya/likelihoods/sn/roman_c.py
+++ Cocoa/cobaya/cobaya/likelihoods/sn/roman_c.py
@@ -1,7 +1,7 @@
 from cobaya.likelihoods._base_classes import _sn_prototype
 
 
-class roman_c(SN):
+class roman_c(_sn_prototype):
     r"""
     Likelihood (marginalized over nuisance parameters) of the JLA type Ia supernova sample
     \cite{Betoule:2014frx}, based on observations obtained by the SDSS-II and SNLS
